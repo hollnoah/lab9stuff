5a4b2aca822d259b9f12bcdc219f62b2b4014668
Noah Holloway <noahholloway23@gmail.com>
2025-11-06 09:40:43 -0700

    stuff and things

--- lab9stuff/lab9stuff.vb
+++ lab9stuff/lab9stuff.vb
@@ -3,6 +3,9 @@ Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 
 
 Public Class lab9stuff
+    Private receivedBytes As New List(Of Byte)
+    Private closingForm As Boolean = False
+
     Sub PopulatePorts()
         Dim portNames() As String = SerialPort.GetPortNames()
         Dim previousSelection As String = If(ComboBox1.SelectedItem IsNot Nothing, ComboBox1.SelectedItem.ToString(), String.Empty)
@@ -104,12 +107,78 @@ Public Class lab9stuff
     End Sub
 
     '-------------------------------------------------EVENT HANDLERS----------------------------------------------------------------------------------------------------------
-    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
-        CheckForIllegalCrossThreadCalls = False
+    'Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+    '    CheckForIllegalCrossThreadCalls = False
 
-        Dim dataReceived As String = SerialPort1.ReadExisting()
-        TextBox1.Text = dataReceived
+    '    Dim byteMyData(SerialPort1.BytesToRead - 1) As Byte
+    '    SerialPort1.Read(byteMyData, 0, byteMyData.Length)
+
+    '    ' We expect 2 bytes for ADC
+    '    If byteMyData.Length >= 2 Then
+    '        Dim highByte As Integer = byteMyData(0)
+    '        Dim lowByte As Integer = byteMyData(1)
+    '        Dim adcValue As Integer = (highByte * 256) + lowByte
+    '        TextBox1.Text = adcValue.ToString()
+    '    End If
+
+    '    'Dim dataReceived As String = SerialPort1.ReadExisting()
+    '    'TextBox1.Text = dataReceived
+    'End Sub
+
+    'Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+    '    Dim buffer(SerialPort1.BytesToRead - 1) As Byte
+    '    SerialPort1.Read(buffer, 0, buffer.Length)
+
+    '    ' Temporary debug: just print all incoming bytes
+    '    Dim debugString As String = ""
+    '    For Each b In buffer
+    '        debugString &= b.ToString("X2") & " "
+    '    Next
+    '    Me.Invoke(Sub() TextBox1.Text = debugString)
+    'End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        If closingForm Then Return ' Ignore if form is closing
+
+        Dim buffer(SerialPort1.BytesToRead - 1) As Byte
+        SerialPort1.Read(buffer, 0, buffer.Length)
+
+        SyncLock receivedBytes
+            receivedBytes.AddRange(buffer)
+        End SyncLock
+
+        ' Invoke UI update safely
+        If Not closingForm Then
+            Try
+                Me.Invoke(Sub() ProcessReceivedData())
+            Catch ex As Exception
+                ' Ignore errors if form is closing
+            End Try
+        End If
     End Sub
+
+    Private Sub ProcessReceivedData()
+        SyncLock receivedBytes
+            While receivedBytes.Count >= 2
+                Dim highByte As Integer = receivedBytes(0)
+                Dim lowByte As Integer = receivedBytes(1)
+                Dim adcValue As Integer = (highByte << 8) Or lowByte
+
+                TextBox1.Text = adcValue.ToString()
+
+                receivedBytes.RemoveRange(0, 2)
+            End While
+        End SyncLock
+    End Sub
+
+
+    Private Sub lab9stuff_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        closingForm = True
+        If SerialPort1.IsOpen Then SerialPort1.Close()
+    End Sub
+
+
+
 
     Private Sub ComboBox1_DropDown(sender As Object, e As EventArgs) Handles ComboBox1.DropDown
         ' Refresh list when the user opens the dropdown
@@ -123,16 +192,16 @@ Public Class lab9stuff
         Write()
     End Sub
 
-    Private Sub ReadDataTimer_Tick(sender As Object, e As EventArgs) Handles ReadDataTimer.Tick
-        Dim byteMyData(SerialPort1.BytesToRead) As Byte
-        Dim temp As String
-        SerialPort1.Read(byteMyData, 0, SerialPort1.BytesToRead)
-        For Each thing In byteMyData
-            temp &= Chr(thing)
-        Next
-        Me.Text = temp
+    'Private Sub ReadDataTimer_Tick(sender As Object, e As EventArgs) Handles ReadDataTimer.Tick
+    '    Dim byteMyData(SerialPort1.BytesToRead) As Byte
+    '    Dim temp As String
+    '    SerialPort1.Read(byteMyData, 0, SerialPort1.BytesToRead)
+    '    For Each thing In byteMyData
+    '        temp &= Chr(thing)
+    '    Next
+    '    Me.Text = temp
 
-    End Sub
+    'End Sub
 
     Private Sub TrackBar1_ValueChanged(sender As Object, e As EventArgs) Handles TrackBar1.ValueChanged
         Write()
@@ -146,15 +215,14 @@ Public Class lab9stuff
             MessageBox.Show("serial port not open")
         End If
 
-        ' --- Read back the two ADC bytes ---
-        Dim highByte As Integer = SerialPort1.ReadByte()
-        Dim lowByte As Integer = SerialPort1.ReadByte()
-
-        ' --- Combine into 10-bit ADC value ---
-        Dim adcValue As Integer = (highByte * 256) + lowByte
-
-        ' --- Display result ---
-        TextBox1.Text = adcValue.ToString()
+        'If SerialPort1.BytesToRead >= 2 Then
+        '    Dim highByte As Integer = SerialPort1.ReadByte()
+        '    Dim lowByte As Integer = SerialPort1.ReadByte()
+        '    Dim adcValue As Integer = (highByte * 256) + lowByte
+        '    TextBox1.Text = adcValue.ToString()
+        'End If
     End Sub
+
+
 
 End Class
